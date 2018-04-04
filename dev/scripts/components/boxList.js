bb07c63ecc6a72eea7bf5766860800f63511397d
Anastasiya Melentyeva <anastasya.melenteva@gmail.com>
2018-04-04 17:30:44 -0400

    Initial commit

---
+++ dev/scripts/components/boxList.js
@@ -0,0 +1,18 @@
+import React, { Component} from 'react';
+
+class BoxList extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        }
+    }
+
+    render() {
+        return(
+            <div>was up?</div>
+        )
+    }
+}
+
+export default BoxList;
---
+++ dev/scripts/components/itemsList.js
@@ -0,0 +1,18 @@
+import React, { Component} from 'react';
+
+class ItemsList extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        }
+    }
+
+    render() {
+        return(
+            <div>was up?</div>
+        )
+    }
+}
+
+export default ItemsList;
